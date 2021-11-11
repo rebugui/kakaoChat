165ffc8b3a50535debbbc7dc197e60665964f654
uhyang <bored_enfant@naver.com>
2021-11-12 08:47:12 +0900

    boannews_print 구조 수정

--- kakaoTest.py
+++ kakaoTest.py
@@ -29,7 +29,7 @@ def test():
       "outputs": [
         {
           "simpleText": {
-            "text": "멍청아"
+            "text": "멍청이"
           }
         }
       ]
@@ -42,90 +42,88 @@ def boannews_print():
     content = request.get_json()
     content = content['userRequest']
     content = content['utterance']
-
-    if content == "보안뉴스":
-        dataSend = {
-          "version": "2.0",
-          "template": {
-            "outputs": [
-              {
-                "carousel": {
-                  "type": "basicCard",
-                  "items": [
+    dataSend = {
+      "version": "2.0",
+      "template": {
+        "outputs": [
+          {
+            "carousel": {
+              "type": "basicCard",
+              "items": [
+                {
+                  "title": boannews.news_title_[0],
+                  "thumbnail": {
+                    "imageUrl": boannews.news_img_[0]
+                  },
+                  "buttons": [
                     {
-                      "title": boannews.news_title_[0],
-                      "thumbnail": {
-                        "imageUrl": boannews.news_img_[0]
-                      },
-                      "buttons": [
-                        {
-                          "action": "webLink",
-                          "label": "구경하기",
-                          "webLinkUrl": boannews.news_link_[0]
-                        }
-                      ]
-                    },
-                    {
-                      "title": boannews.news_title_[1],
-                      "thumbnail": {
-                        "imageUrl": boannews.news_img_[1]
-                      },
-                      "buttons": [
-                        {
-                          "action": "webLink",
-                          "label": "구경하기",
-                          "webLinkUrl": boannews.news_link_[1]
-                        }
-                      ]
-                    },
-                    {
-                      "title": boannews.news_title_[2],
-                      "thumbnail": {
-                        "imageUrl": boannews.news_img_[2]
-                      },
-                      "buttons": [
-                        {
-                          "action": "webLink",
-                          "label": "구경하기",
-                          "webLinkUrl": boannews.news_link_[2]
-                        }
-                      ]
-                    },
-                    {
-                      "title": boannews.news_title_[3],
-                      "thumbnail": {
-                        "imageUrl": boannews.news_img_[3]
-                      },
-                      "buttons": [
-                        {
-                          "action": "webLink",
-                          "label": "구경하기",
-                          "webLinkUrl": boannews.news_link_[3]
-                        }
-                      ]
-                    },
-                    {
-                      "title": boannews.news_title_[4],
-                      "thumbnail": {
-                        "imageUrl": boannews.news_img_[4]
-                      },
-                      "buttons": [
-                        {
-                          "action": "webLink",
-                          "label": "구경하기",
-                          "webLinkUrl": boannews.news_link_[4]
-                        }
-                      ]
-                    },
+                      "action": "webLink",
+                      "label": "구경하기",
+                      "webLinkUrl": boannews.news_link_[0]
+                    }
                   ]
-                }
-              }
-            ]
+                },
+                {
+                  "title": boannews.news_title_[1],
+                  "thumbnail": {
+                    "imageUrl": boannews.news_img_[1]
+                  },
+                  "buttons": [
+                    {
+                      "action": "webLink",
+                      "label": "구경하기",
+                      "webLinkUrl": boannews.news_link_[1]
+                    }
+                  ]
+                },
+                {
+                  "title": boannews.news_title_[2],
+                  "thumbnail": {
+                    "imageUrl": boannews.news_img_[2]
+                  },
+                  "buttons": [
+                    {
+                      "action": "webLink",
+                      "label": "구경하기",
+                      "webLinkUrl": boannews.news_link_[2]
+                    }
+                  ]
+                },
+                {
+                  "title": boannews.news_title_[3],
+                  "thumbnail": {
+                    "imageUrl": boannews.news_img_[3]
+                  },
+                  "buttons": [
+                    {
+                      "action": "webLink",
+                      "label": "구경하기",
+                      "webLinkUrl": boannews.news_link_[3]
+                    }
+                  ]
+                },
+                {
+                  "title": boannews.news_title_[4],
+                  "thumbnail": {
+                    "imageUrl": boannews.news_img_[4]
+                  },
+                  "buttons": [
+                    {
+                      "action": "webLink",
+                      "label": "구경하기",
+                      "webLinkUrl": boannews.news_link_[4]
+                    }
+                  ]
+                },
+              ]
+            }
           }
-          
-        }
-    
+        ]
+      }
+    }
     return jsonify(dataSend)
+    
+    
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)                      
