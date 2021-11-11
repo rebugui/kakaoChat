4f5cdf2811b510a926a2583037165e920583c31d
uhyang <bored_enfant@naver.com>
2021-11-11 17:50:19 +0900

    보안뉴스 관련 오류 수정

--- boannews.py
+++ boannews.py
@@ -8,17 +8,17 @@ news_title_= ["","","","",""]
 news_link_= ["","","","",""]
 news_img_= ["","","","",""]
 
-for i in range(1,6):
-    news_title_[i] = soup.find("li",{"onmouseover":"headline_news(%d);"%i}).find("p").text
+for i in range(0,5):
+    news_title_[i] = soup.find("li",{"onmouseover":"headline_news(%d);"%(i+1)}).find("p").text
 
-for i in range(1,6):
-    news_link_[i] = soup.find("li",{"onmouseover":"headline_news(%d);"%i}).get('onclick')
+for i in range(0,5):
+    news_link_[i] = soup.find("li",{"onmouseover":"headline_news(%d);"%(i+1)}).get('onclick')
     news_link_[i] = news_link_[i].replace("location.href='","")
     news_link_[i] = news_link_[i].replace("';","")
     news_link_[i] = "https://www.boannews.com/"+news_link_[i]
 
-for i in range(1,6):
-    news_img_[i] = soup.find('div', id='headline%d'%(i-1)).find(class_='headline_big_img').get('style')
+for i in range(0,5):
+    news_img_[i] = soup.find('div', id='headline%d'%i).find(class_='headline_big_img').get('style')
     news_img_[i] = news_img_[i].replace("background: #fff url(","")
     news_img_[i] = news_img_[i].replace(") center center no-repeat;background-size:cover;","")
     news_img_[i] = "https://www.boannews.com/"+news_img_[i]
