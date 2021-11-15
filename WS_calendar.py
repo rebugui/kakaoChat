b838a883ce656a12fcbe07313a5c1b2efb5eba07
Yang uhyeok <51498462+rebugui@users.noreply.github.com>
2021-11-15 22:38:50 +0900

    Add files via upload

---
+++ WS_calendar.py
@@ -0,0 +1,17 @@
+import requests, ssl
+from bs4 import BeautifulSoup
+ssl._create_default_https_context = ssl._create_unverified_context
+
+main_url = 'https://www.woosuk.ac.kr/classScheduleList.do?mzcode=K00M0409&gubun=UN'
+res = requests.get(main_url,verify=False)
+soup = BeautifulSoup(res.content, 'lxml')
+date_t = []
+day_t = []
+
+date = soup.select('#month_iljung > dl > dd.date_info > span')
+for t in date:
+    date_t.append(t.text)
+
+day = soup.select('#month_iljung > dl > dd.day_info > span')
+for t in day:
+    day_t.append(t.text)
