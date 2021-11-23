aebc3cc648502a631650c10c3b4e6417bbaab495
Yang uhyeok <bored_enfant@naver.com>
2021-11-23 20:51:21 +0900

    .

--- kakaoChat_main.py
+++ kakaoChat_main.py
@@ -236,20 +236,20 @@ def ws_calendar():
       ]
     }    
   }
-  for i in range(10): 
+  for i in range(0,5): 
     dataSend['template']['outputs'][0]['carousel']['items'][0]['itemList'].append(
       {
         "title": WS_calendar.day_t[i],
         "description": WS_calendar.date_t[i],
       }
     )
-  #for i in range(10,20): 
-  #  dataSend['template']['outputs'][0]['carousel']['items'][1]['itemList'].append(
-  #    {
-  #      "title": WS_calendar.day_t[i],
-  #      "description": WS_calendar.date_t[i],
-  #    }
-  #  )
+  for i in range(5,10): 
+    dataSend['template']['outputs'][0]['carousel']['items'][1]['itemList'].append(
+      {
+        "title": WS_calendar.day_t[i],
+        "description": WS_calendar.date_t[i],
+      }
+    )
   return jsonify(dataSend)
 
 if __name__ == "__main__":
