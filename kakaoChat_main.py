a70d88abc38fa7809c3aa7da802943283abb8730
Yang uhyeok <bored_enfant@naver.com>
2021-11-23 20:45:14 +0900

    .

--- kakaoChat_main.py
+++ kakaoChat_main.py
@@ -237,14 +237,14 @@ def ws_calendar():
     }    
   }
   for i in range(0,10): 
-    dataSend['template']['outputs'][0]['itemCard']['items'][0]['itemList'].append(
+    dataSend['template']['outputs'][0]['carousel']['items'][0]['itemList'].append(
       {
         "title": WS_calendar.day_t[i],
         "description": WS_calendar.date_t[i],
       }
     )
   for i in range(10,20): 
-    dataSend['template']['outputs'][0]['itemCard']['items'][1]['itemList'].append(
+    dataSend['template']['outputs'][0]['carousel']['items'][1]['itemList'].append(
       {
         "title": WS_calendar.day_t[i],
         "description": WS_calendar.date_t[i],
