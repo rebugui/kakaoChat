0a976a3e6e501bca60fd1ae744a1363e92f5b5e1
Yang uhyeok <bored_enfant@naver.com>
2021-11-24 22:31:00 +0900

    .

--- kakaoChat_main.py
+++ kakaoChat_main.py
@@ -303,7 +303,7 @@ def ws_calendar():
   else:
     dataSend['template']['outputs'][0]['carousel']['items'][0]['items'].append(
       {
-        "title": "%d월 학사일정이 없습니다."%ymonth,
+        "title": "%s월 학사일정이 없습니다."%ymonth,
       }
     )    
   return jsonify(dataSend)
