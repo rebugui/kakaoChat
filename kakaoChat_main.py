35b7e65ed7ad919868dad178afc14bcad925caaa
Yang uhyeok <bored_enfant@naver.com>
2021-11-25 20:39:09 +0900

    .

--- kakaoChat_main.py
+++ kakaoChat_main.py
@@ -181,7 +181,7 @@ def ws_calendar():
   item_count = (len(date_t) // 5)
   if (len(date_t) % 5) > 0:
     item_count = item_count + 1
-  if date_t == None:
+  if date_t == 0:
     dataSend['template']['outputs'][0]['carousel']['items'].append(
         {
           "header": {
