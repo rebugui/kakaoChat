497f9716aabcd38871412379362de4920fbd904f
uhyang <bored_enfant@naver.com>
2021-12-31 13:14:56 +0900

    .

--- KrCheck.py
+++ KrCheck.py
@@ -1,20 +1,18 @@
 from hanspell import spell_checker
  
-input = u‘안녕 하세요. 저는 한국인 입니다.’
+input = u'안녕 하세요오오오오엉'
  
-input_convert = input.replace(‘.’,‘.#’).split(‘#’)
+input_convert = input.replace('.','.#').split('#')
  
-input_list =  [“”]
+input_list =  [""]
  
 for i in input_convert:
     print(i)
-    if len(input_list[–1]) + len(i) < 500:
-        input_list[–1] += i
+    if len(input_list[-1]) + len(i) < 500:
+        input_list[-1] += i
     else:
         input_list.append(i)  
  
 result = spell_checker.check(input_list)
  
-print(result[0].result)
- 
-# true
+print(result[0].result
