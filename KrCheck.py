f28863d3e3d871b21b13bba3e4f4282737c0725a
yuh <bored_enfant@naver.com>
2021-12-31 16:25:43 +0900

    .

--- KrCheck.py
+++ KrCheck.py
@@ -1,11 +1,10 @@
 from hanspell import spell_checker
- 
-input = u'안녕 하세요오오오오엉'
- 
+input = "안녕 하세요오오오오엉"
 input_convert = input.replace('.','.#').split('#')
  
 input_list =  [""]
- 
+output_list = [""]
+
 for i in input_convert:
     print(i)
     if len(input_list[-1]) + len(i) < 500:
@@ -14,5 +13,8 @@ for i in input_convert:
         input_list.append(i)  
  
 result = spell_checker.check(input_list)
- 
-print(result[0].result
+
+for i in result.checked:
+    output_list.append(i)
+
+print(output_list[i])
