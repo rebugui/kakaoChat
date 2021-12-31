0d1c81e09669dffd1a9b71ecea5899a39140979f
yuh <bored_enfant@naver.com>
2021-12-31 16:50:29 +0900

    .

--- KrCheck.py
+++ KrCheck.py
@@ -1,18 +1,28 @@
 # -*- coding: utf-8 -*-
 
 from hanspell import spell_checker
-input = "안녕하세여 양우혁 입미다."
 
-input_convert = input.replace('.','.#').split('#')
-input_list =  [""]
+def krcheck(input)
+    input_convert = input.replace('.','.#').split('#')
+    input_list =  [""]
+    
+    for i in input_convert:
+        print(i)
+        if len(input_list[-1]) + len(i) < 500:
+            input_list[-1] += i
+        else:
+            input_list.append(i)
+    
+    result = spell_checker.check(input_list)
 
-for i in input_convert:
-    print(i)
-    if len(input_list[-1]) + len(i) < 500:
-        input_list[-1] += i
-    else:
-        input_list.append(i)
+    return input_convert,result
 
-result = spell_checker.check(input_list)
+def main():
+    input = "의미는 메인 함수의 선언, 시작을 의미합니다. 해당 코드 밑에 main 등의 함수 호출 코드를 작성해서 함수의 기능을 수행합니다.의미는 메인 함수의 선언, 시작을 의미합니다. 해당 코드 밑에 main 등의 함수 호출 코드를 작성해서 함수의 기능을 수행합니다.의미는 메인 함수의 선언, 시작을 의미합니다. 해당 코드 밑에 main 등의 함수 호출 코드를 작성해서 함수의 기능을 수행합니다."
+    input_convert,result = krcheck(input)
+    print(input_convert) 
+    print(result) 
 
-print(result[0].checked)
+
+if __name__ == "__main__":
+	main()
