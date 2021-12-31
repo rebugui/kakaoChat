6da4bd6d38fd11452dc43980e7e35d4dd14ebea1
uhyang <bored_enfant@naver.com>
2021-12-31 13:07:22 +0900

    .

--- KrCheck.py
+++ KrCheck.py
@@ -1,6 +1,20 @@
 from hanspell import spell_checker
-
-sent = "외않되"
-spelled_sent = spell_checker.check(sent)
-
-print(spelled_sent)
+ 
+input = u‘안녕 하세요. 저는 한국인 입니다.’
+ 
+input_convert = input.replace(‘.’,‘.#’).split(‘#’)
+ 
+input_list =  [“”]
+ 
+for i in input_convert:
+    print(i)
+    if len(input_list[–1]) + len(i) < 500:
+        input_list[–1] += i
+    else:
+        input_list.append(i)  
+ 
+result = spell_checker.check(input_list)
+ 
+print(result[0].result)
+ 
+# true
