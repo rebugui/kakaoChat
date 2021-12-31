f0449219d1bee07c60e087c91c17e0d546235bb6
yuh <bored_enfant@naver.com>
2021-12-31 17:37:37 +0900

    .

--- KrCheck.py
+++ KrCheck.py
@@ -20,8 +20,11 @@ def krcheck(input):
 def main():
     input = '의미는 메인 함수의 선언, 시작을 의미합니다. 해당 코드 밑에 main 등의 함수 호출 코드를 작성해서 함수의 기능을 수행합니다.의미는 메인 함수의 선언, 시작을 의미합니다. 해당 코드 밑에 main 등의 함수 호출 코드를 작성해서 함수의 기능을 수행합니다.의미는 메인 함수의 선언, 시작을 의미합니다. 해당 코드 밑에 main 등의 함수 호출 코드를 작성해서 함수의 기능을 수행합니다.'
     result = krcheck(input)
-    print(result[0].checked)
-
+    print("검사 전: "+result[0].original)
+    print("검사 성공 여부: "result[0].result)
+    print("검사 후: "result[0].checked)
+    print("애러 갯수: "result[0].errors)
+    print("진행 시간: "result[0].time)
 
 
 if __name__ == "__main__":
