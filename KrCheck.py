da31f0bb39e9ad3d7164b56da123a45a6b06b9f6
yuh <bored_enfant@naver.com>
2022-01-01 19:12:21 +0900

    .

--- KrCheck.py
+++ KrCheck.py
@@ -4,8 +4,8 @@ from hanspell import spell_checker
 def KrCheck(input):
     input_convert = input.replace('.','.#').split('#')
     input_list =  [""]
-    time = 0
-
+    running_time = 0
+    errors_count = 0
     for i in input_convert:
         if len(input_list[-1]) + len(i) < 500:
             input_list[-1] += i
@@ -14,8 +14,8 @@ def KrCheck(input):
     
     output_text = spell_checker.check(input_list)
     for i in output_text:
-        errors_count = errors_count + output_text.errors
-        running_time = running_time + output_text.time
+        errors_count = errors_count + int(output_text.errors)
+        running_time = running_time + int(output_text.time)
         output_text.append(output_text.checked)
 
     return output_text,errors_count,running_time
