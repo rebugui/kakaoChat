2bcc82770bb548bc8263a9cf587517c126d505c7
yuh <bored_enfant@naver.com>
2022-01-01 19:21:22 +0900

    .

--- KrCheck.py
+++ KrCheck.py
@@ -3,10 +3,10 @@ from hanspell import spell_checker
 
 def KrCheck(input):
     input_convert = input.replace('.','.#').split('#')
-    input_list,output_text =  [""],[""]
+    input_list =  [""]
     running_time = 0
     errors_count = 0
-
+    output_text = ""
     for i in input_convert:
         if len(input_list[-1]) + len(i) < 500:
             input_list[-1] += i
@@ -14,12 +14,9 @@ def KrCheck(input):
             input_list.append(i)
     
     output_text = spell_checker.check(input_list)
-    for output_text in output_text:
-        errors_count = errors_count + int(output_text.errors)
-        running_time = running_time + int(output_text.time)
-        output_text.append(str(output_text.checked))
-    
-
-    output_text = ''.join(output_text)
+    for i in output_text:
+        errors_count = errors_count + int(i.errors)
+        running_time = running_time + int(i.time)
+        output_text = output_text + i.checked
 
     return output_text,errors_count,running_time
