19825368b945a727d9ce22bca5fc38796395686f
yuh <bored_enfant@naver.com>
2022-01-04 21:07:35 +0900

    번역기능 테스트

--- lang_Translator.py
+++ lang_Translator.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
 from googletrans import Translator
 
-def lang_Translator(input_text):
-    translator = Translator()
+def lang_Translator():
 
-    if translator.detect(input_text).lang == 'kr':
-        result = translator.translate(text1, src='kr', dest='en')    
+    input_text = "한국어를 입력하면 영어로 나옵니다."
+    translator = Translator()
+    
+    lang = translator.detect(input_text).lang
+
+    if lang == 'kr':
+        result = translator.translate(text1, src=lang, dest='en')    
     else:
-        result = translator.translate(text1, src='en', dest='kr')
-        
-    return result
+        result = translator.translate(text1, src=lang, dest='kr')
+    
+    print(output_text)
+#    return output_text
