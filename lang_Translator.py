2c35f369725953ac2af64bf599a2c855654faaf3
uhyang <bored_enfant@naver.com>
2022-01-05 09:34:50 +0900

    .

--- lang_Translator.py
+++ lang_Translator.py
@@ -8,7 +8,7 @@ translator = Translator()
 
 lang = translator.detect(input_text).lang
 if lang == 'kr':
-    output_text = translator.translate(input_text, src='%s'%lang, dest='en')    
+    output_text = translator.translate(input_text, src='kr', dest='en')    
 else:
     output_text = translator.translate(input_text, src=lang, dest='kr')
 
