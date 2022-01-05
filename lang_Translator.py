57714e3b87ca6af6e323a8569ec98b573e99e38e
uhyang <bored_enfant@naver.com>
2022-01-05 09:36:28 +0900

    .

--- lang_Translator.py
+++ lang_Translator.py
@@ -6,11 +6,10 @@ from googletrans import Translator
 input_text = "요건 한글이에영."
 translator = Translator()
 
-lang = translator.detect(input_text).lang
-if lang == 'kr':
-    output_text = translator.translate(input_text, src='kr', dest='en')    
+if translator.detect(input_text).lang == 'kr':
+    output_text = translator.translate(input_text, src=translator.detect(input_text).lang, dest='en')    
 else:
-    output_text = translator.translate(input_text, src=lang, dest='kr')
+    output_text = translator.translate(input_text, src=translator.detect(input_text).lang, dest='kr')
 
 print(output_text)
 #    return output_text
