3c9bb3e4073f6c18f874fe4021afb42f327412d5
uhyang <bored_enfant@naver.com>
2022-01-05 09:37:37 +0900

    .

--- lang_Translator.py
+++ lang_Translator.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 from googletrans import Translator
 
-#def lang_Translator():
+def lang_Translator():
 
-input_text = "요건 한글이에영."
-translator = Translator()
+    input_text = "이건 한글입니당"
+    translator = Translator()
+    
+    lang = translator.detect(input_text).lang
 
-if translator.detect(input_text).lang == 'kr':
-    output_text = translator.translate(input_text, src=translator.detect(input_text).lang, dest='en')    
-else:
-    output_text = translator.translate(input_text, src=translator.detect(input_text).lang, dest='kr')
-
-print(output_text)
+    if lang == 'kr':
+        output_text = translator.translate(input_text, src=lang, dest='en')    
+    else:
+        output_text = translator.translate(input_text, src=lang, dest='kr')
+    
+    print(output_text)
 #    return output_text
